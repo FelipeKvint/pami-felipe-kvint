331cb228874e0435ea61810239e8bd474cc6ecbd
Felipe <lipe.almagrok@gmail.com>
2026-09-10 17:30:25 -0300

    aula 1

---
+++ aula01/introducao.js
@@ -0,0 +1,13 @@
+console.log("Olá mundo");
+
+
+var Variavel01 = "Oi,";
+let Variavel02 = "tudo  bem?";
+const Variavel03 = "Vocês são  lindos";
+
+//Concatenação  de  variaveis
+console.log(Variavel01 + Variavel02); // sem espaço
+console.log(Variavel01, Variavel02);  // insere espaço
+
+Variavel01 = "Olá";
+console.log(Variavel01, Variavel02); 
